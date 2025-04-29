15f9e2a1974d255bc6705175e9cc0e60a8ae6a92
Paras Shah <parasshah195@gmail.com>
2025-04-29 21:19:29 +0530

    Add gsap lib
    since it is not yet available in Webflow for this project

--- src/global.ts
+++ src/global.ts
@@ -1,6 +1,11 @@
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+
 import { setCurrentYear } from '$utils/current-year';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
   setCurrentYear();
+
+  gsap.registerPlugin(ScrollTrigger);
 });
