a4e202fc5b11bc817a0844636d5f715691c30b71
Paras Shah <parasshah195@gmail.com>
2025-04-29 23:51:11 +0530

    Load gsap directly in Webflow globally

--- src/global.ts
+++ src/global.ts
@@ -1,11 +1,6 @@
-import gsap from 'gsap';
-import ScrollTrigger from 'gsap/ScrollTrigger';
-
 import { setCurrentYear } from '$utils/current-year';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
   setCurrentYear();
-
-  gsap.registerPlugin(ScrollTrigger);
 });
