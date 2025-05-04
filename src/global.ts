7681a6cd5041fbe1095895bb5b8cce4a6a14ba9d
Paras Shah <parasshah195@gmail.com>
2025-05-04 10:43:34 +0530

    Add default gsap ease

--- src/global.ts
+++ src/global.ts
@@ -4,6 +4,7 @@ import '$utils/scroll-smoother';
 window.Webflow ||= [];
 window.Webflow.push(() => {
   gsap.registerPlugin(ScrollTrigger);
+  gsap.defaults({ ease: 'power3.out' });
 
   setCurrentYear();
 });
