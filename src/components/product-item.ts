c1b21ada69d83641094d203087c9e041ebaab762
Paras Shah <parasshah195@gmail.com>
2025-05-26 02:19:46 +0530

    Reload Finsweet fields on loading new Shopify data
    Cleanup scrolltrigger on filtering/pagination
    Fix animation not working on fast clicks of filters

--- src/components/product-item.ts
+++ src/components/product-item.ts
@@ -1,13 +1,38 @@
 export class ProductItem {
   PRODUCT_ITEM_SELECTOR = '[data-el="product-item"]';
 
+  private productItemScrollTriggers: ScrollTrigger[][] = [];
+
   constructor() {
+    this.refetchFinsweetFilterData();
     this.loadShopyflowOnCMSList();
     this.addStaggerAnimation();
   }
 
+  private refetchFinsweetFilterData() {
+    window.addEventListener('ShopyflowReady', (event) => {
+      Shopyflow.on('collectionModuleReady', ({ el, collection }) => {
+        window.FinsweetAttributes ||= [];
+        window.FinsweetAttributes.push([
+          'list',
+          (listInstances) => {
+            const [listInstance] = listInstances;
+
+            if (!listInstances.length) return;
+
+            console.log('listInstance items', listInstance.items);
+
+            listInstance.items.value.forEach((item) => {
+              item.collectFields();
+            });
+          },
+        ]);
+      });
+    });
+  }
+
   private addStaggerAnimation() {
-    this.setInitialAnimState(this.PRODUCT_ITEM_SELECTOR);
+    // this.setInitialAnimState(this.PRODUCT_ITEM_SELECTOR);
     this.addProductItemAnimation(this.PRODUCT_ITEM_SELECTOR);
   }
 
@@ -23,17 +48,25 @@ export class ProductItem {
         window.IS_DEBUG_MODE && console.debug('listInstance', listInstance);
 
         listInstance.addHook('beforeRender', (items) => {
+          if (!items.length) return;
+
           const listItemElList = items.map((item) => item.element);
+          // Kill previous scrolltriggers that may not have rendered
+          this.productItemScrollTriggers.flat().forEach((trigger) => trigger.kill());
+          this.productItemScrollTriggers = [];
+
           this.setInitialAnimState(listItemElList);
-        });
-
-        listInstance.addHook('render', () => {
-          window.Shopyflow.fetchNew();
-        });
-
-        listInstance.addHook('afterRender', (items) => {
-          const listItemElList = items.map((item) => item.element);
           this.addProductItemAnimation(listItemElList);
+        });
+
+        listInstance.addHook('pagination', (items) => {
+          if (!items.length) return;
+
+          window.Shopyflow.fetchNew().then(() => {
+            items.forEach((item) => {
+              item.collectFields();
+            });
+          });
         });
       },
     ]);
@@ -50,15 +83,16 @@ export class ProductItem {
   }
 
   private addProductItemAnimation(target: gsap.DOMTarget) {
-    ScrollTrigger.batch(target, {
+    const batchTriggers = ScrollTrigger.batch(target, {
       onEnter: (batch) => {
         gsap.to(batch, {
-          opacity: 1,
+          autoAlpha: 1,
           y: 0,
           stagger: 0.15,
         });
       },
       start: 'top bottom-=100px',
     });
+    this.productItemScrollTriggers.push(batchTriggers);
   }
 }
